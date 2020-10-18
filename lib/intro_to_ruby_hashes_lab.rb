d2589a2667991ea050a0bbfefce2ea9d0ba4cf5c
Alexa Botello <abotello20@yahoo.com>
2020-10-18 21:30:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,5 @@
-def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
-end
+def my_hash_creator(hash, key)
+
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
